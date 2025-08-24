f490bb7f7bdd29fb6732d57f6dcff67307a767ab
Jean-Baptiste Perrier <jeanbapt.perrier@gmail.com>
2025-08-24 21:10:22 +0200

    fix: use correct types in lib.c

--- thirdparty/lib.c
+++ thirdparty/lib.c
@@ -1,6 +1,6 @@
 #include "koto.h"
 
-KOTO_API int koto_load(void* module, size_t map) {
+KOTO_API int koto_load(Module module, ValueId map) {
     ValueId str_value = koto_create_str(module, "Hello from Koto!");
     ValueId num_value = koto_create_number(module, 42.5);
     koto_map_insert(module, map, "greeting", str_value);
