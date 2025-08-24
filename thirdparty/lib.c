5620414a9c1014439e52d048239c2cf13050d853
Jean-Baptiste Perrier <jeanbapt.perrier@gmail.com>
2025-08-24 21:09:03 +0200

    feat: add inner map example

--- thirdparty/lib.c
+++ thirdparty/lib.c
@@ -5,5 +5,10 @@ KOTO_API int koto_load(void* module, size_t map) {
     ValueId num_value = koto_create_number(module, 42.5);
     koto_map_insert(module, map, "greeting", str_value);
     koto_map_insert(module, map, "answer", num_value);
+
+    ValueId inner_map = koto_create_map(module);
+    ValueId inner_str = koto_create_str(module, "Inner value");
+    koto_map_insert(module, inner_map, "inner_key", inner_str);
+    koto_map_insert(module, map, "inner_map", inner_map);
     return 0;
 }
